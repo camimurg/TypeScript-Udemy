0651c42471cf1cb9896d3486befdd9fdcf752e5f
Camilla Murgia <camillamurgia@3522GLOBAL.homenet.telecomitalia.it>
2021-04-30 13:17:09 +0200

    play around with object and tupple

--- basics-05-object-types/app.ts
+++ basics-05-object-types/app.ts
@@ -2,9 +2,32 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  numbers: number[];
+  randomArray: any[];
+  role: [number, string]; // TUPPLE
+} = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
+  hobbies: ['sports', 'cooking'], // string[];
+  numbers: [1, 2, 3], // number[];
+  randomArray: ['miao', 2, true], // (string | number | boolean)[] or any[];
+  role: [2, 'author']
 };
 
+person.role.push('admin'); // it doesn't get an error...
+person.role = [0, 'admin', 'user']; // you get an error
+console.log(person.role)
+// person.role[1] = 10;
+
+let favoriteActivities: any[];
+favoriteActivities = ['sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
