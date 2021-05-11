c8127556990a13c13a65483783292c441b8b9e76
Camilla Murgia <camillamurgia@3522GLOBAL.local>
2021-05-11 08:44:30 +0200

    reorganized folder

--- section2_basics/basics-12-function-types/app.ts
+++ section2_basics/basics-12-function-types/app.ts
@@ -26,3 +26,15 @@ console.log(combineValues(8, 8));
 addAndHandle(10, 20, (result) => {
   console.log(result);
 });
+
+
+
+function sendRequest(data: string, cb: (response: any) => void) {
+  // ... sending a request with "data"
+  return cb({data: 'Hi there!'});
+}
+ 
+sendRequest('Send this!', (response) => { 
+  console.log(response);
+  return true;
+ });
