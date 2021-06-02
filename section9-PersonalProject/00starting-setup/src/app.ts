b781b09843eec3196675f8497f7ee8015f1feccb
Camilla Murgia <camillamurgia@3522GLOBAL.local>
2021-06-02 17:45:16 +0200

    create project list class

--- section9-PersonalProject/00starting-setup/src/app.ts
+++ section9-PersonalProject/00starting-setup/src/app.ts
@@ -51,7 +51,37 @@ function autobind(
   return adjDescriptor;
 }
 
+// ProjectList class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
 
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    //import the contect
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-project`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement; //OR HTMLElement: <div id='app'><div/>
@@ -108,14 +138,11 @@ class ProjectInput {
       required: true,
       minLength: 1
     };
+
     if (
       !validate(titleValidatable) ||
       !validate(descriptionValidatable) ||
       !validate(peopleValidatable)
-
-      // enteredTitle.trim().length === 0 ||
-      // enteredDescription.trim().length === 0 ||
-      // enteredPeople.trim().length === 0 
       ) {
         alert('Invalid input, please try again');
         return;
@@ -146,3 +173,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
