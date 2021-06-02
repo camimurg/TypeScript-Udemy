cafa03b8371a2685da2ee1c94ebb0f8c36899dfc
Camilla Murgia <camillamurgia@3522GLOBAL.local>
2021-06-02 19:29:13 +0200

    filtering projects with enums

--- section9-PersonalProject/00starting-setup/src/app.ts
+++ section9-PersonalProject/00starting-setup/src/app.ts
@@ -9,7 +9,7 @@ class Project {
     public title: string,
     public description: string,
     public people: number,
-    public state: ProjectStatus
+    public status: ProjectStatus
     ) {}
 }
 
@@ -127,7 +127,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(prj => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -137,6 +143,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-project-list`)! as HTMLUListElement;
+    listEl.innerHTML = ''; // get rid of all the items and then rerender
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
