7168b2def86989d588156a70004ba525c4ac899b
Camilla Murgia <camillamurgia@3522GLOBAL.local>
2021-06-04 12:26:20 +0200

    add getter for project item

--- section9-PersonalProject/00starting-setup/src/app.ts
+++ section9-PersonalProject/00starting-setup/src/app.ts
@@ -158,6 +158,15 @@ function autobind(
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  // add a getter
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -170,7 +179,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.project.people.toString();
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
