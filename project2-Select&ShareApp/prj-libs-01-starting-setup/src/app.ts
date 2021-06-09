8d68aea884adc7940a3c4a0547668338cdcebb81
Camilla Murgia <camillamurgia@3522GLOBAL.local>
2021-06-09 14:32:21 +0200

    add a bit of style and start the function to get user input

--- project2-Select&ShareApp/prj-libs-01-starting-setup/src/app.ts
+++ project2-Select&ShareApp/prj-libs-01-starting-setup/src/app.ts
@@ -1,1 +1,13 @@
 // Code goes here!
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(evt: Event) {
+  evt.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  //send this to Google's API!
+
+}
+
+form.addEventListener('submit', searchAddressHandler);
